5f5012f6bba4ffa5fa84970fb74bf0f4a62ec81b
khuseyn-khasiev <khuseyn-khasiev@mail.ru>
2023-08-21 15:23:53 +0300

    редактирования данных о себе

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,5 @@
 import './AboutMe.css';
-import myPhoto from '../../images/my-photo.png';
+import myPhoto from '../../images/my-photo.jpg';
 export default function AboutMe() {
     return (
         <section className='about-me' id='about-me-id'>
@@ -7,10 +7,9 @@ export default function AboutMe() {
                 <h2 className='about-me__title'>Студент</h2>
                 <div className='about-me__biography'>
                     <div className='about-me__info'>
-                        <h3 className='about-me__name'>Виталий</h3>
+                        <h3 className='about-me__name'>Хусейн</h3>
                         <p className='about-me__subtitle'>Фронтенд-разработчик, 30 лет</p>
-                        <p className='about-me__text'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-                            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+                        <p className='about-me__text'>Специалист по информационным технологиям в образовании. Закончил курсы по "Веб-разработке". После чего ушел с основной работы и начал участвовать в различных проектах.  Увлекаюсь спортивными и настольными играми.</p>
                         <a className='about-me__subtext' href='https://github.com/khuseynkhasiev' target='_blank'>Github</a>
                     </div>
                     <img className='about-me__img' src={myPhoto} alt='my-photo' />
